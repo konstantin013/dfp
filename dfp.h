9d85fbc4c113e1ef2db992d4ad8e0deafab47f2d
konstantin <konstantin_013@mail.ru>
2017-10-20 11:48:34 +0300

    create definition for one dimensional search and dfp method

--- dfp.h
+++ dfp.h
@@ -5,4 +5,12 @@
 #ifndef DFP_DFP_H
 #define DFP_DFP_H
 
+#include "linear-algebra.h"
+#include "function.h"
+
+typedef double (*One_dim_search)(Function f, Point x, Point d);
+
+Point
+dfp(Function f, Point x0, One_dim_search s);
+
 #endif //DFP_DFP_H
